7d076f076d09fb53e6014a55f10ed15d2607679c
Adeyeye Olalekan <adeyeye.lekan01@gmail.com>
2022-12-22 23:30:09 +0100

    changed map marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-// import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import iconMarker from "/public/assets/images/icon-location.svg";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 const Map = ({ lat = 13.54232, long = 0.72934 }) => {
+  const icon = L.icon({
+    iconRetinaUrl: iconRetina,
+    iconUrl: iconMarker,
+    shadowUrl: iconShadow,
+    iconSize: [30, 35],
+    iconAnchor: [12, 41],
+  });
+
   return (
     <MapContainer center={[lat, long]} zoom={13} scrollWheelZoom={false}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[lat, long]}>
+      <Marker icon={icon} position={[lat, long]}>
         <Popup>
           Where you at! <br />
           {lat}, {long}
