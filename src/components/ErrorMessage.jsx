0ae95d5b02a5836ef966168cfdb1fb947664902d
Adeyeye Olalekan <adeyeye.lekan01@gmail.com>
2022-12-23 14:38:08 +0100

    edited error message

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -6,7 +6,6 @@ const ErrorMessage = () => {
       <div className="center">
         <h4>Error loading OpenMap. Check for the following:</h4>
         <ul>
-          <li>Geolocation is available/enabled</li>
           <li>Active network connection</li>
         </ul>
       </div>
